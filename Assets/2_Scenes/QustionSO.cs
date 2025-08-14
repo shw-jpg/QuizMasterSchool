d6f2478520de422e3ee13a38cb9de253aaf42b1a
서효원 <vahyeon2502@sonline20.sen.go.kr>
2025-08-14 11:00:47 +0900

    56. 배열

--- Assets/2_Scenes/QustionSO.cs
+++ Assets/2_Scenes/QustionSO.cs
@@ -1,8 +1,21 @@
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "Quiz Question", fileName = "Nwq Auestion")]
+[CreateAssetMenu(menuName = "Quiz Question", fileName = "New Question")]
 public class QuestionSO : ScriptableObject
 {
     [TextArea(2, 6)]
     [SerializeField] string question = "여기에 질문을 적어주세요";
+    [SerializeField] string[] answers = new string[4];
+
+    public string GetQuestion()
+    {
+        return question;
+    }
+
+    public string Getanswer(int undex)
+    {
+        return answers[undex];
+    }
+
+
 }
