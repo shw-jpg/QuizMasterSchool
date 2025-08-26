794b2d75db5282341175f74584f5735894a6f34f
서효원 <vahyeon2502@sonline20.sen.go.kr>
2025-08-26 10:57:47 +0900

    60.버튼 상태

--- Assets/2_Scenes/Quiz.cs
+++ Assets/2_Scenes/Quiz.cs
@@ -12,6 +12,18 @@ public class Quiz : MonoBehaviour
 
     void Start()
     {
+        GetNxtQuestion();
+    }
+
+    private void GetNxtQuestion()
+    {
+        OnDisplayQuestion();
+        SetDefsultButtonSprites();
+        SetButtoState(true);
+    }
+
+    private void OnDisplayQuestion()
+    {
         questionText.text = question.GetQuestion();
 
         for (int i = 0; i < answerButtonArr.Length; i++)
@@ -26,6 +38,27 @@ public class Quiz : MonoBehaviour
         {
             questionText.text = "정답입니다!";
             answerButtonArr[index].GetComponent<Image>().sprite = correctAnswerSprite;
+        }
+        else
+        {
+            questionText.text = "틀렸습니다!" + question.GetCorrectAnswer();
+        }
+        SetButtoState(false);
+    }
+
+    private void SetDefsultButtonSprites()
+    {
+        foreach (GameObject obj in answerButtonArr)
+        {
+            obj.GetComponent<Image>().sprite = defaultAnswerSprite;
+        }
+    }
+
+    private void SetButtoState(bool state)
+    {
+        foreach (GameObject obj in answerButtonArr)
+        {
+            obj.GetComponent<Button>().interactable = state;
         }
     }
 }
