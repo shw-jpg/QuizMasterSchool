5f37f1d3d8a7c1d583241217e57dd2d691f2c757
서효원 <vahyeon2502@sonline20.sen.go.kr>
2025-09-05 14:36:40 +0900

    a1.델리게이트와 이벤트

---
+++ Assets/2_Scenes/Test/Program.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class Program : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Hello World");
+        Publisher publisher = new Publisher();
+        publisher.msg += ResultProcess;
+        publisher.msg += OtherProcess;
+
+        publisher.SendMessage("추가 문제주세요!");
+
+        Debug.Log("작업 완료");
+    }
+
+    void ResultProcess(string msg)
+    {
+        Debug.Log($"메시지 수신: {msg}");
+    }
+
+    void OtherProcess(string text)
+    {
+        Debug.Log($"다른 처리: {text}");
+    }
+}
+
+public class Publisher
+{
+    public delegate void OnMessage(string msg);
+    public event OnMessage msg;
+
+    public void SendMessage(string text)
+    {
+        Debug.Log($"ChatGPT API와 통신합니다.(1분 걸림)... {text}");
+
+        msg?.Invoke(text);
+    }
+}
